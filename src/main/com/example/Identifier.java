48f5a73370556f792c22fc4925cc4f130776a634
Gourabh <gourabhpatel2798@gmail.com>
2025-07-28 09:36:05 +0530

    Identifier class

--- src/main/com/example/Identifier.java
+++ src/main/com/example/Identifier.java
@@ -1,5 +1,17 @@
 package src.main.com.example;
 
 public class Identifier {
+    int alpahNumeric;//alphabet and numberic are allow in java and 2 special character are allow ($,_)
 
+    //    int 9abc;// not allow should be started alphabet or special character
+    int _abc;
+    int $abc;
+
+    //Case sencetive
+    int a;
+    int A;
+
+    //not used predefine keyword
+//    int do;
+//    int continue;
 }
